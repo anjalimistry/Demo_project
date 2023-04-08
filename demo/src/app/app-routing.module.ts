3beabc9a2f5f64f9f6867890463ccddf3fb2c583
LAPTOP-GU8PNMGT\pares <bhandarianjali1995@gmail.com>
2023-04-08 12:34:43 +0530

    summary view and detail view display data

--- demo/src/app/app-routing.module.ts
+++ demo/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SummaryViewComponent } from './summary-view/summary-view.component';
+import { DetailsViewComponent } from './details-view/details-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SummaryViewComponent },
+  { path: 'detailview', component: DetailsViewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SummaryViewComponent } from './summary-view/summary-view.component';
+import { DetailsViewComponent } from './details-view/details-view.component';
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryViewComponent,
+    DetailsViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
